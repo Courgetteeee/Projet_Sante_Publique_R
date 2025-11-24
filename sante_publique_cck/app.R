5a7799770a1587dd46af4f901eddc4d801322f64
Clara Gambardello <clara.gambardello@gmail.com>
2025-11-24 13:48:32 +0100

    ui fini !

--- sante_publique_cck/app.R
+++ sante_publique_cck/app.R
@@ -7,15 +7,35 @@
 #    https://shiny.posit.co/
 #
 
+# Les packages
+
 library(shiny)
+library(ggplot2)
+library(dplyr)
+library(readxl)
+
+# Importation des données
+
+df<-read_excel("../data/Inf_lib_2012_2023.xlsx", sheet=2)
+df
+
+# Traitement des données
+
+medecin<-read_excel("data/Med_2012_2025.xlsx", sheet=2)
+
+medecin_clean <- medecin %>% filter(!(substr(territoire, 1, 1) %in% c("0", "3")), region != "00-Ensemble", 
+                                    sexe=="0-Ensemble", departement!="000-Ensemble", exercice == "0-Ensemble",
+                                    tranche_age=="00-Ensemble", specialites != "00-Ensemble")
 
 
-#Clara et Cindy c'est le squelette de l'app shiny
-# ok karla merci
+medecin_long <- medecin_clean %>% pivot_longer(cols=starts_with("effectif_"),
+  names_to = "annee", values_to = "effectif") %>% 
+  mutate(annee=as.integer(sub("effectif_", "", annee)))
 
 
 
-#ABCDEFGHIJKLMNOPQRSTUVWXYZ
+
+# ---------------------------- UI -----------------------------------
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
@@ -26,19 +46,25 @@ ui <- fluidPage(
     # Sidebar with a slider input for number of bins 
     sidebarLayout(
         sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
+          selectInput(inputId="departement", label="Choisir un département :", choices=medecin_long$departement, 
+                      selected=medecin_long$departement[1]),
+          selectInput(inputId="specialites", label="Choisir une spécialités :", choices=medecin_long$specialites, 
+                      selected=medecin_long$specialites[1]),
+          downloadLink('downloadData', 'Download')
         ),
 
         # Show a plot of the generated distribution
         mainPanel(
-           plotOutput("distPlot")
+          tabsetPanel(
+            tabPanel("Evolution des effectifs",
+                     plotOutput("effectifs_medecin")
+            ),
+          )
         )
     )
 )
+
+# ---------------------------- SERVER -----------------------------------
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
@@ -54,6 +80,8 @@ server <- function(input, output) {
              main = 'Histogram of waiting times')
     })
 }
+
+# ---------------------------- GLOBAL -----------------------------------
 
 # Run the application 
 shinyApp(ui = ui, server = server)
